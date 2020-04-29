2290c9170063edb2e4f0d0c04b8f96a930e3b154
Chandan Kumar <chandan061184@outlook.com>
2020-04-29 18:59:06 +0530

    Promise.all

--- Promises.js
+++ Promises.js
@@ -35,8 +35,24 @@ function createPost(post) {
     
 }
 
-createPost({title:'Post three', body:'This is post three'})
-.then(getPost)
-.catch((error)=>console.log(error));
+// createPost({title:'Post three', body:'This is post three'})
+// .then(getPost)
+// .catch((error)=>console.log(error));
+
+//Promise.all
+
+const promise1 = Promise.resolve('Hello World!');
+const promise2 =  10;
+const promise3 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('Good bye!');
+    }, 2000);
+});
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+.then((res)=>res.json());
+
+Promise.all([promise1, promise2, promise3, promise4])
+.then((values)=>console.log(values));
 
 

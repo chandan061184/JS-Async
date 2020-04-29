bc98b9d00b1327fecf3511fa704d19f4d9b17098
Chandan Kumar <chandan061184@outlook.com>
2020-04-29 18:09:47 +0530

    Without callback

---
+++ Callback.js
@@ -0,0 +1,32 @@
+const posts = [
+    { title: 'Post one', body: 'This is post one' },
+    { title: 'Post two', body: 'This is post two' }
+]
+
+//()=>{}
+function getPost() {
+//To mimic server lets say server takes 1 sec to get all posts
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+
+    }, 1000);
+    
+}
+
+function createPost(post)
+{
+    //To mimic server lets say server takes 2 sec to create a post
+    setTimeout(()=>{
+        posts.push(post)}
+        , 2000);
+}
+
+createPost({title:'Post three', body:'This is post three'});
+getPost(); //getPost() will get executed and will paint the DOM even before createPost completed
+
+
+

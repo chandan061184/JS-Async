f615562f49e9925b84ea7aae0d05c7c1ffe4b018
Chandan Kumar <chandan061184@outlook.com>
2020-04-29 18:20:22 +0530

    Adding Callback

--- Callback.js
+++ Callback.js
@@ -17,16 +17,30 @@ function getPost() {
     
 }
 
-function createPost(post)
+// function createPost(post)
+// {
+//     //To mimic server lets say server takes 2 sec to create a post
+//     setTimeout(()=>{
+//         posts.push(post)}
+//         , 2000);
+// }
+
+// createPost({title:'Post three', body:'This is post three'});
+// getPost(); //getPost() will get executed and will paint the DOM even before createPost completed
+
+
+//Callback
+
+function createPost(post, Callback)
 {
     //To mimic server lets say server takes 2 sec to create a post
     setTimeout(()=>{
-        posts.push(post)}
-        , 2000);
+        posts.push(post);
+        Callback();
+    }, 2000);
 }
-
-createPost({title:'Post three', body:'This is post three'});
-getPost(); //getPost() will get executed and will paint the DOM even before createPost completed
+//Callback getPost() method: Now getPost will get executed after posts.push(post) statement of method createPost()
+createPost({title:'Post three', body:'This is post three'}, getPost);
 
 
 
